025ff26b8a370d862596a617bae05c11c65fe83d
legend524 <thecrazydev524@gmail.com>
2023-11-14 11:05:38 +0900

    bsc complete

--- scripts/deployment-zap.ts
+++ scripts/deployment-zap.ts
@@ -28,31 +28,60 @@ async function main() {
         config.wbnb
     );
 
-    console.log('set token type for usdt');
+    console.log("set token type for usdt");
     await zapper.setTokenType(config.usdt, 1);
-    console.log('set token type for busd');
+    console.log("set token type for busd");
     await zapper.setTokenType(config.busd, 1);
-    console.log('set token type for usdc');
+    console.log("set token type for usdc");
     await zapper.setTokenType(config.usdc, 1);
-    // await zapper.setTokenType(config.wild, 1);
-    
-    // usdt - wbnb
-    console.log('set swap path for usdt-wbnb');
+
+    // wbnb - wild
+    console.log("set swap path for usdt-wbnb");
+    await zapper.setSwapPath(config.wild, config.wbnb, [config.wild, config.wbnb]);
+    await zapper.setSwapPath(config.wbnb, config.wild, [config.wbnb, config.wild]);
+
+    // wbnb - usdc
+    console.log("set swap path for usdt-wbnb");
+    await zapper.setSwapPath(config.usdc, config.wbnb, [config.usdc, config.wbnb]);
+    await zapper.setSwapPath(config.wbnb, config.usdc, [config.wbnb, config.usdc]);
+
+    // wbnb - busd
+    console.log("set swap path for usdt-wbnb");
+    await zapper.setSwapPath(config.busd, config.wbnb, [config.busd, config.wbnb]);
+    await zapper.setSwapPath(config.wbnb, config.busd, [config.wbnb, config.busd]);
+
+    // wbnb - usdt
+    console.log("set swap path for usdt-wbnb");
     await zapper.setSwapPath(config.usdt, config.wbnb, [config.usdt, config.wbnb]);
     await zapper.setSwapPath(config.wbnb, config.usdt, [config.wbnb, config.usdt]);
-    
+
     // wild - usdt
-    console.log('set swap path for usdt-wild');
+    console.log("set swap path for usdt-wild");
     await zapper.setSwapPath(config.wild, config.usdt, [config.wild, config.wbnb, config.usdt]);
     await zapper.setSwapPath(config.usdt, config.wild, [config.usdt, config.wbnb, config.wild]);
 
+    // wild - usdc
+    console.log("set swap path for usdt-wild");
+    await zapper.setSwapPath(config.wild, config.usdc, [config.wild, config.wbnb, config.usdc]);
+    await zapper.setSwapPath(config.usdc, config.wild, [config.usdc, config.wbnb, config.wild]);
+
+    // wild - busd
+    console.log("set swap path for usdt-wild");
+    await zapper.setSwapPath(config.wild, config.busd, [config.wild, config.wbnb, config.busd]);
+    await zapper.setSwapPath(config.busd, config.wild, [config.busd, config.wbnb, config.wild]);
+
     // usdc - usdt
-    console.log('set swap path for usdt-usdc');
+    console.log("set swap path for usdt-usdc");
     await zapper.setSwapPath(config.usdc, config.usdt, [config.usdc, config.wbnb, config.usdt]);
     await zapper.setSwapPath(config.usdt, config.usdc, [config.usdt, config.wbnb, config.usdc]);
 
+    // usdc - busd
+    console.log("set swap path for usdt-usdc");
+    await zapper.setSwapPath(config.usdc, config.busd, [config.usdc, config.wbnb, config.busd]);
+    await zapper.setSwapPath(config.busd, config.usdc, [config.busd, config.wbnb, config.usdc]);
+
     // usdt - busd
-    console.log('set swap path for usdt-busd');
+    console.log("set swap path for usdt-busd");
     await zapper.setSwapPath(config.busd, config.usdt, [config.busd, config.wbnb, config.usdt]);
     await zapper.setSwapPath(config.usdt, config.busd, [config.usdt, config.wbnb, config.busd]);
 
--- scripts/settings.ts
+++ scripts/settings.ts
@@ -6,12 +6,12 @@ const config = {
     router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
     feeAddress: "0xD7544E32014F4D0ad77bf5084C8Cbd2321b7a930",
     deployerAddress: "0x600bE5FcB9338BC3938e4790EFBeAaa4F77D6893",
-    wild: "0xE9f49BE8C684A823102b33DA0cEa82c6e229F05C",
+    wild: "0xC5BDB5b8f5BDc6eD6251B250C69a20e329d66282",
     usdc: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
     weth: "0x4DB5a66E937A9F4473fA95b1cAF1d1E1D62E29EA",
     wbnb: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-    zapper: "0x8C0Df30e63135eC82bBE0ABb8B28fEE078bAbc50",
-    masterchef: "0x2b2Ed985F0F6C5EB1B1942e53c24E060fE43537b",
+    zapper: "0x449d14e5D2C439a235E28A65CA1744a96355CF93",
+    masterchef: "0x6D80e16B6B44D9d69c1fB535326Dee44B06594bA",
 };
 
 async function main() {
@@ -22,16 +22,19 @@ async function main() {
 
     const token = await ethers.getContractAt("ThreeWildToken", config.wild);
     // const zapper = await ethers.getContractAt("ThreeWildToken", config.zapper);
-    const masterchef = await ethers.getContractAt("ThreeWildToken", config.masterchef);
-
+    const masterchef = await ethers.getContractAt("MasterChef", config.masterchef);
 
     // await token.mint(config.feeAddress, ethers.utils.parseEther("5000"));
 
     // console.log("setting zapper to whitelist...");
     // await token.setProxy(config.zapper);
     // console.log("done");
-    console.log("transferring ownership to masterchef...");
-    await token.transferOwnership(config.masterchef);
+    // console.log("transferring ownership to masterchef...");
+    // await token.transferOwnership(config.masterchef);
+    // console.log("done");
+
+    console.log("updating emission...");
+    await masterchef.updateEmissionRate("11000000000000000");
     console.log("done");
 
     // console.log("transferring ownership of masterchef...");
